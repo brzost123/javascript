f615ff846df5cb6747d6c3e6dbe739bc79cacadd
brzost123 <39437305+brzost123@users.noreply.github.com>
2018-06-14 10:43:34 +0200

    Updates script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function Phone(brand, price, color, height, width, weight) {
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
+  this.brand = brand;
+  this.price = price;
+  this.color = color;
   this.height = height;
   this.width = width;
   this.weight = weight;
 }
 Phone.prototype.printInfo = function() {
-	console.log(
+    console.log(
     "The phone brand is " + this.brand + 
     ", color is " + this.color + 
     ", price is " + this.price + 
     ", height is " + this.height + 
     ", width is " + this.width + 
     " and the weight is " + this.weight + 
-    ".");
+    "."
+    );
 }
 
 var iPhone6S = new Phone("Apple", 2250, "silver", "110 mm ", "50 mm", "100 g");
